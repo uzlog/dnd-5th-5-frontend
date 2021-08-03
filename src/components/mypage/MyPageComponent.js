8e0c385cae8ab37e5ad4cb292239500ff9a810f8
junseo <ysj6844@naver.com>
2021-08-03 12:31:01 +0900

    Feature: 모달 알라카드 1. 알라카드 클릭 시, 전체 모달로 전환 + 모달에서 배경 입히는데, 짧은 딜레이가 있음 #14

--- src/components/mypage/MyPageComponent.js
+++ src/components/mypage/MyPageComponent.js
@@ -4,26 +4,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 import { ModalWrapper, ModalOverlay, ModalContents } from '@components/main/Style';
 
-const Contents = styled.div`
+const ContentsWrapper = styled.div`
+  display: table;
   width: 312px;
-  height: 290px;
+  height: 420px;
   margin-left: 24px;
   margin-right: 24px;
-  background-color: red;
-  margin-top: 65px;
-  /* margin-bottom: 89px; */
   line-height: 1.6;
   letter-spacing: -0.5px;
-  text-align: left;
-  font-size: 28px;
+  font-size: 36px;
   font-weight: 300;
+  color: black;
+`;
+
+const InnerContents = styled.div`
+  display: table-cell;
+  vertical-align: middle;
+  text-align: left;
 `;
 
 const MoreButtonWrapper = styled.div`
   width: 360px;
   height: 64px;
-  /* margin: 0 0 65px;
-  padding: 20px 24px 20px 312px; */
   display: flex;
   align-items: center;
   justify-content: flex-end;
@@ -45,7 +47,6 @@ const MoreButtonInner = styled.div`
   width: 10px;
   height: 10px;
   border-radius: 10px;
-  /* margin: 0 0 0 5px; */
   border: solid 5px var(--primary-color-white);
   background-color: white;
 `;
@@ -55,7 +56,7 @@ const ButtonWrapper = styled.div`
   align-items: center;
   justify-content: flex-end;
   margin-right: 24px;
-  margin-top: 89px;
+  height: 96px;
 `;
 
 const StyledButton = styled.button`
@@ -64,7 +65,6 @@ const StyledButton = styled.button`
   padding: 11px 28px;
   border-radius: 62px;
   cursor: pointer;
-  /* background-color: transparent; */
   border: solid 1px var(--primary-color-white);
 `;
 
@@ -74,18 +74,32 @@ const Header = styled.div`
   background-color: blue;
   color: white;
 `;
+
+const CustomModalWrapper = styled(ModalWrapper)`
+  display: table;
+  line-height: 1.6;
+  letter-spacing: -0.5px;
+  font-size: 36px;
+  font-weight: 300;
+  color: black;
+`;
+const CustomModalContents = styled(ModalContents)`
+  /* display: table-cell;
+  vertical-align: middle;
+  text-align: left; */
+`;
 const MyPageComponent = ({ state }) => {
   const { alacardData } = state;
   const [showModal, setShowModal] = useState(false);
+  const [sentence, setSentence] = useState('');
 
-  const openModal = () => {
+  const openModal = (card) => {
     setShowModal(true);
-    console.log('open');
+    setSentence(card.target.innerHTML);
   };
 
   const closeModal = () => {
     setShowModal(false);
-    console.log('close');
   };
   return (
     <>
@@ -100,12 +114,15 @@ const MyPageComponent = ({ state }) => {
         }
 
         card.sentence = card.sentence.replaceAll(', ', ',<br />');
-        card.sentence += '!';
+        if (!card.sentence.includes('!')) {
+          card.sentence += '!';
+        }
+        console.log(card.sentence);
         return (
           <>
             <div
               key={idx}
-              onClick={() => openModal()}
+              onClick={(card) => openModal(card)}
               style={{
                 backgroundImage: 'url(' + backgroundImgUrl + ')',
                 backgroundSize: '360px 580px',
@@ -115,26 +132,12 @@ const MyPageComponent = ({ state }) => {
               }}>
               <MoreButtonWrapper>
                 <MoreButton>
-                  {/* <MoreButtonInner />
-                <MoreButtonInner />
-                <MoreButtonInner /> */}
                   <FontAwesomeIcon icon={faEllipsisH} size="1x" />
                 </MoreButton>
               </MoreButtonWrapper>
-              <Contents dangerouslySetInnerHTML={{ __html: card.sentence }} />
-              {/* <Contents>
-              {card.sentence.split(', ').map((s, idx) =>
-                card.selectedWordList.length - 1 > idx ? (
-                  <>
-                    {s}, <br />
-                  </>
-                ) : (
-                  <>
-                    {s}! <br />
-                  </>
-                ),
-              )}
-            </Contents> */}
+              <ContentsWrapper>
+                <InnerContents dangerouslySetInnerHTML={{ __html: card.sentence }} />
+              </ContentsWrapper>
               <ButtonWrapper>
                 <StyledButton>키워드 PICK 요청하기</StyledButton>
               </ButtonWrapper>
@@ -142,7 +145,21 @@ const MyPageComponent = ({ state }) => {
             {showModal && (
               <ModalWrapper>
                 <ModalOverlay onClick={() => closeModal()} />
-                <ModalContents>{card.sentence}</ModalContents>
+                <CustomModalContents
+                  style={{
+                    backgroundImage: 'url(' + backgroundImgUrl + ')',
+                    backgroundSize: '360px 640px',
+                    width: '360px',
+                    height: '640px',
+                    color: 'black',
+                    display: 'table',
+                    lineHeight: '1.6',
+                    letterSpacing: '-0.5px',
+                    fontSize: '36px',
+                    fontWeight: '300',
+                  }}>
+                  <InnerContents dangerouslySetInnerHTML={{ __html: sentence }} />
+                </CustomModalContents>
               </ModalWrapper>
             )}
           </>
