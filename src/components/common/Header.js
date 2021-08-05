a678086ff2a48f7c8555d066499f3556115fa8a3
junseo <ysj6844@naver.com>
2021-08-05 20:20:18 +0900

    Feature: 헤더 1. 기본 UI 구현 #15

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import logo from '@assets/img/nav/logo.svg';
+import friend from '@assets/img/nav/friend.svg';
+import activatedNotice from '@assets/img/nav/activatedNotice.svg';
+import inactivatedNotice from '@assets/img/nav/inactivatedNotice.svg';
+import avatar from '@assets/img/nav/avatar.svg';
 
 const Wrapper = styled.div`
-  /* background-color: #121212; */
-  background-color: pink;
-  /* display: flex;
-  align-items: center; */
+  background-color: #121212;
   @media screen and (max-width: 767px) {
     width: 360px;
     height: 60px;
@@ -15,30 +17,44 @@ const Wrapper = styled.div`
 
 const LogoWrapper = styled.div`
   float: left;
+  display: flex;
+  align-items: center;
+  @media screen and (max-width: 767px) {
+    height: 24px;
+    width: 46px;
+  }
 `;
 
 const IconWrapper = styled.div`
-  width: 92px;
+  width: 112px;
   float: right;
   display: flex;
   align-items: center;
   div:not(:last-child) {
-    margin-right: 21px;
+    margin-right: 20px;
   }
 `;
 
-const TempSpan = styled.div`
+const ImgWrapper = styled.div`
   width: 24px;
 `;
 
 const Header = () => {
   return (
     <Wrapper>
-      <LogoWrapper>Logo</LogoWrapper>
+      <LogoWrapper>
+        <img src={logo} width="37px" height="19px" alt="로고" />
+      </LogoWrapper>
       <IconWrapper>
-        <TempSpan>1</TempSpan>
-        <TempSpan>2</TempSpan>
-        <TempSpan>3</TempSpan>
+        <ImgWrapper>
+          <img src={friend} width="24px" height="24px" alt="친구창" />
+        </ImgWrapper>
+        <ImgWrapper>
+          <img src={inactivatedNotice} width="24px" height="24px" alt="알림창" />
+        </ImgWrapper>
+        <ImgWrapper>
+          <img src={avatar} width="24px" height="24px" alt="프로필 사진" />
+        </ImgWrapper>
       </IconWrapper>
     </Wrapper>
   );
