7c999c667cfec21a729ca19aee4ee5009519f105
junseo <ysj6844@naver.com>
2021-09-06 21:33:08 +0900

    Fix: 로고 png로 수정

--- src/components/main/MainComponent.js
+++ src/components/main/MainComponent.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { ModalWrapper, ModalOverlay, ModalContents } from './Style';
 import SocialLoginContainer from '@containers/auth/SocialLoginContainer';
 // import logo from '@assets/img/logo/alaLogo.svg';
-import logo from '@assets/img/ala.svg';
+import logo from '@assets/img/ala.png';
 
 const Wrapper = styled.div`
   width: 100vw;
